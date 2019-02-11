66e8b8cbdbaeac76125de790d50081c47834ed58
Sushmetha <46591896+Sushmetha@users.noreply.github.com>
2019-02-11 01:01:42 -0800

    Create set1vorc.py

---
+++ set1vorc.py
@@ -0,0 +1,8 @@
+string = input()
+if string.isalpha():
+	if string == 'a' or string =='e' or string =='i' or string == 'o'or string == 'u':
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
