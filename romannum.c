6078b5ea642a5c7cf6bd751a5d998103e9a3c526
Sushmetha <46591896+Sushmetha@users.noreply.github.com>
2019-06-08 00:14:39 -0700

    Rename roman.c to romannum.c

---
+++ romannum.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+int digitvalue(char);
+ 
+int main(void) {
+	char roman;
+	scanf("%s",&roman);
+	if(roman<=20){
+		switch(roman){
+ 
+			case 'I':
+			printf("1");
+			break;
+			case 'II':
+			printf("2");
+			break;
+			case 'III':
+			printf("3");
+			break;
+			case 'IV':
+			printf("4");
+			break;
+			case 'V':
+			printf("5");
+			break;
+			case 'VI':
+			printf("6");
+			break;
+			case 'VII':
+			printf("7");
+			break;
+			case 'VIII':
+			printf("8");
+			break;
+			case 'IX':
+			printf("9");
+			break;
+			case 'X':
+			printf("10");
+			break;
+			case 'XI':
+			printf("11");
+			break;
+			case 'XII':
+			printf("12");
+			break;
+			case 'XIII':
+			printf("13");
+			break;
+			case 'XIV':
+			printf("14");
+			break;
+			case 'XV':
+			printf("15");
+			break;
+			case 'XVI':
+			printf("16");
+			break;
+			case 'XVII':
+			printf("17");
+			break;
+			case 'XVIII':
+			printf("18");
+			break;
+			case 'XIX':
+			printf("19");
+			break;
+			case 'XX':
+			printf("20");
+			break;
+		}
+	}
+ 
+ 
+ 
+	return 0;
+}#include <stdio.h>
+int digitvalue(char);
+ 
+int main(void) {
+	char roman;
+	scanf("%s",&roman);
+	if(roman<=20){
+		switch(roman){
+ 
+			case 'I':
+			printf("1");
+			break;
+			case 'II':
+			printf("2");
+			break;
+			case 'III':
+			printf("3");
+			break;
+			case 'IV':
+			printf("4");
+			break;
+			case 'V':
+			printf("5");
+			break;
+			case 'VI':
+			printf("6");
+			break;
+			case 'VII':
+			printf("7");
+			break;
+			case 'VIII':
+			printf("8");
+			break;
+			case 'IX':
+			printf("9");
+			break;
+			case 'X':
+			printf("10");
+			break;
+			case 'XI':
+			printf("11");
+			break;
+			case 'XII':
+			printf("12");
+			break;
+			case 'XIII':
+			printf("13");
+			break;
+			case 'XIV':
+			printf("14");
+			break;
+			case 'XV':
+			printf("15");
+			break;
+			case 'XVI':
+			printf("16");
+			break;
+			case 'XVII':
+			printf("17");
+			break;
+			case 'XVIII':
+			printf("18");
+			break;
+			case 'XIX':
+			printf("19");
+			break;
+			case 'XX':
+			printf("20");
+			break;
+		}
+	}
+ 
+ 
+ 
+	return 0;
+}#include <stdio.h>
+int digitvalue(char);
+ 
+int main(void) {
+	char roman;
+	scanf("%s",&roman);
+	if(roman<=20){
+		switch(roman){
+ 
+			case 'I':
+			printf("1");
+			break;
+			case 'II':
+			printf("2");
+			break;
+			case 'III':
+			printf("3");
+			break;
+			case 'IV':
+			printf("4");
+			break;
+			case 'V':
+			printf("5");
+			break;
+			case 'VI':
+			printf("6");
+			break;
+			case 'VII':
+			printf("7");
+			break;
+			case 'VIII':
+			printf("8");
+			break;
+			case 'IX':
+			printf("9");
+			break;
+			case 'X':
+			printf("10");
+			break;
+			case 'XI':
+			printf("11");
+			break;
+			case 'XII':
+			printf("12");
+			break;
+			case 'XIII':
+			printf("13");
+			break;
+			case 'XIV':
+			printf("14");
+			break;
+			case 'XV':
+			printf("15");
+			break;
+			case 'XVI':
+			printf("16");
+			break;
+			case 'XVII':
+			printf("17");
+			break;
+			case 'XVIII':
+			printf("18");
+			break;
+			case 'XIX':
+			printf("19");
+			break;
+			case 'XX':
+			printf("20");
+			break;
+		}
+	}
+ 
+ 
+ 
+	return 0;
+}
