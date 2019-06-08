58c8e21cee3a5af0e2a9b83ff2e2476ed3ecf78c
Sushmetha <46591896+Sushmetha@users.noreply.github.com>
2019-06-08 00:29:43 -0700

    Create romnumber.py

---
+++ romnumber.py
@@ -0,0 +1,4 @@
+input =input()
+str=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+if input in str:
+  print(str.index(input))
